043c55a11c261bd41a01dda5ee856b65aa697281
Lemony666 <17865557598@qq.com>
2021-10-08 11:41:48 -0400

    Update visualizing_function.R

--- Assignment2/visualizing_function.R
+++ Assignment2/visualizing_function.R
@@ -1,19 +1,23 @@
-country_compare <- function(data){ #build a function, and the only interest is an input dataset
-  ggplot(data= data, aes(x=year, y=log(value), colour = country))+ #choosing data, x lab, ylab and country is group
-    geom_line() + #plot a line chart
-    facet_wrap(~id)+ #divided chart by which group they are
-    geom_label_repel(data= subset(data, year == max(year)), #set for label text and the site in the end of the line
+country_compare <- function(data1, country_choose){ #build a function, and the only interest is an input dataset
+  data2 <- data1[data1$country %in% country_choose, ]
+  ggplot(data= data2, aes(x=year, y=value, colour = country))+ #choosing data, x lab, ylab and country is group
+    geom_line() +
+    facet_wrap(~id)+#plot a line chart
+    geom_label_repel(data= subset(data2, year == max(year)), #set for label text and the site in the end of the line
                      aes(label = country), #label by country
                      nudge_x = .75, #the label will far from
                      na.rm = TRUE) +
-    theme(legend.position = "none")}
+    theme(legend.position = "none")
+  }
 
-id_compare <- function(data){
-  ggplot(data=data, aes(x=year, y=log(value), colour = id))+
+id_compare <- function(data1, country_choose){ #build a function, and the only interest is an input dataset
+  data2 <- data1[data1$country %in% country_choose, ]
+  ggplot(data= data2, aes(x=year, y=value, colour = id))+ #choosing data, x lab, ylab and country is group
     geom_line() +
-    facet_wrap(~country)+ 
-    geom_label_repel(data= subset( data, year == max(year)),
-                     aes(label = country),
-                     nudge_x = .75,
+    facet_wrap(~country)+#plot a line chart
+    geom_label_repel(data= subset(data2, year == max(year)), #set for label text and the site in the end of the line
+                     aes(label = id), #label by id
+                     nudge_x = .75, #the label will far from
                      na.rm = TRUE) +
-    theme(legend.position = "none")}
+    theme(legend.position = "none")
+}
